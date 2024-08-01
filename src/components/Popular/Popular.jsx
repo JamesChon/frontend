5cae3725042e4a3f82e6431d21b4f1464ea4de03
JamesChon <james.chon@sdgku.edu>
2024-07-31 17:21:55 -0700

    7/31

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
     return (
         <div className='popular'>
             <h1>Best Sellers!</h1>
-            <hr />
+
             <div className="popular-item">
                 {data_product.map((item, i) => {
                     return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
